38d0809af5adc5263d04e4817f093dd34f06f2d5
xiaoshshishux <xiaoshishux@163.com>
2023-02-22 00:18:21 +0800

    登录

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -1,34 +1,84 @@
 <template>
-    <div class="form-wrap">
-        <ul class="menu-tab">
-            <li class="current">登录</li>
-            <li>注册</li>
-        </ul>
+    <div id="login">
+
+
+        <div class="form-wrap">
+            <ul class="menu-tab">
+                <li class="current">登录</li>
+                <li>注册</li>
+            </ul>
+            <el-form>
+                <el-form-item>
+                    <label class="form-label">用户名</label>
+                    <el-input />
+                </el-form-item>
+                <el-form-item>
+                    <label class="form-label">密码</label>
+                    <el-input type="password" />
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="onSubmit">Create</el-button>
+                    <el-button>Cancel</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
     </div>
 </template>
 
-<script>
-export default {
-    setup () {
-        
+<script setup>
+import { reactive } from 'vue'
 
-        return {}
-    }
+// do not use same name with ref
+const form = reactive({
+    name: '',
+    region: '',
+    date1: '',
+    date2: '',
+    delivery: false,
+    type: [],
+    resource: '',
+    desc: '',
+})
+
+const onSubmit = () => {
+    console.log('submit!')
 }
+
 </script>
 
 <style lang="scss" scoped>
-.menu-tab{
+#login {
+    height: 100vh;
+    background-color: #344a5f;
+}
+
+.form-wrap {
+    width: 320px;
+    padding-top: 100px;
+    margin: auto;
+}
+
+.menu-tab {
     text-align: center;
-    li{
+
+    li {
         display: inline-block;
         padding: 10px 24px;
         margin: 0 10px;
-        color: red;
+        color: #fff;
         font-size: 14px;
         border-radius: 5px;
         cursor: pointer;
-        &.current{background-color: rgba(0,0,0, .1);}
+
+        &.current {
+            background-color: rgba(0, 0, 0, .1);
+        }
     }
+}
+
+.form-label {
+    display: block;
+    color: #fff;
+    font-size: 14px;
 }
 </style>
