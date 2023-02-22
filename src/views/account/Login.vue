9bf7174a1dadf78b5958e1ff8d1f20bed279e0d6
xiaoshshishux <xiaoshishux@163.com>
2023-02-22 21:11:59 +0800

    高亮状态的逻辑

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -4,8 +4,10 @@
 
         <div class="form-wrap">
             <ul class="menu-tab">
-                <li class="current">登录</li>
-                <li>注册</li>
+                <li :class="{ 'current': current_menu === item.type }" v-for="item in data.tab_menu" :key="item.type"
+                    @click="toggleMenu(item.type)">{{
+                        item.label }}</li>
+                <!-- <li>注册</li> -->
             </ul>
             <el-form>
                 <el-form-item>
@@ -17,8 +19,18 @@
                     <el-input type="password" />
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary" @click="onSubmit">Create</el-button>
-                    <el-button>Cancel</el-button>
+                    <label class="form-label">验证码</label>
+                    <el-row :gutter="10">
+                        <el-col :span="14">
+                            <el-input></el-input>
+                        </el-col>
+                        <el-col :span="10">
+                            <el-button type="success" class="el-button-block">获取验证码</el-button>
+                        </el-col>
+                    </el-row>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="danger" @click="onSubmit" class="el-button-block">登录</el-button>
                 </el-form-item>
             </el-form>
         </div>
@@ -26,7 +38,21 @@
 </template>
 
 <script setup>
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
+
+// data存储的是登录与注册的资源，方便v-for循环遍历出来
+const data = reactive({
+    tab_menu: [
+        { type: "login", label: "登录" },
+        { type: "register", label: "注册" }
+    ]
+})
+// 默认状态的高亮显示，就不会，两项高亮都显示
+let current_menu = ref(data.tab_menu[0].type);
+// 创建点击的事件，来回切换高亮状态
+const toggleMenu = ((type) => {
+    current_menu.value = type
+})
 
 // do not use same name with ref
 const form = reactive({
